78c9f9ae84989ba98fbd7671a2ea93744db7dda0
tuzeyu <tuzeyu@hotmail.com>
2016-02-18 16:39:54 +0800

    Update ViewController.swift
    1.我在网吧改代码，感觉自己萌萌哒。

--- GitHubFirstProject/GitHubFirstProject/ViewController.swift
+++ GitHubFirstProject/GitHubFirstProject/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("hello world")
+        print("我在网吗改代码，感觉自己萌萌哒")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
