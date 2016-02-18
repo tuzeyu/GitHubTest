55c92dbb8091aab38b66f9e67dc81f4e4e816d7c
tuzeyu <tuzeyu@hotmail.com>
2016-02-18 16:37:53 +0800

    1.新建github项目

---
+++ GitHubFirstProject/GitHubFirstProject/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  GitHubFirstProject
+//
+//  Created by 屠泽玉 on 16/2/18.
+//  Copyright © 2016年 屠泽玉. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("hello world")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
